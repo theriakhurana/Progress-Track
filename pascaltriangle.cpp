c61588183407cba801ebbf4473257c572b8909eb
theriakhurana <149751002+theriakhurana@users.noreply.github.com>
2024-01-28 22:14:05 +0530

    Leetcode Question 118 added

---
+++ pascaltriangle.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<vector<int>> generate(int numRows) {
+        vector<vector<int>> result;
+        if(numRows == 1){
+            result.push_back({1});
+            return result;
+        }
+
+        for(int i = 0; i < numRows; i++){
+            vector<int> row(i+1,1);
+            for(int j = 1; j < i; j++){
+                row[j] = result[i-1][j] + result[i-1][j-1];
+            }
+            result.push_back(row);
+        }
+        return result;
+    }
+};
